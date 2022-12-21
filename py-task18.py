34193e416d0bf19a6727099164185d9b6aa8bc28
Alexandr <jwchaganav@gmail.com>
2022-12-21 10:18:11 +0400

    проверил без random

--- py-task18.py
+++ py-task18.py
@@ -22,20 +22,30 @@ try:
     X = 6
     print(X)
     # numbers = []
-    numbers = [3, 3, 5, 4, 2, 3, 5, 1, 7, 8, 2]
-    # for num in range(min,N):
-    #     numbers.append(random.randint(min1,max))
+    numbers = [3, 3, 4, 5, 2, 3, 4, 1, 7, 8, 2]
+    for num in range(min,N):
+        numbers.append(random.randint(min1,max))
     razn = X
     numFind = 0
+    razn2 = X
+    numFind2 = 0
     print(numbers)
     for n in numbers:
-        if X != n:
-            if Valid(X,n, razn):
-                numFind = n 
-                razn = X-n
+        if X != n: # если не равно искомому, то будем проверять
+            if X>n: # поиск по тем что меньше
+                if Valid(X,n, razn):
+                    numFind = n 
+                    razn = X-n
+            else: # поиск по тем что больше
+                if Valid(n,X, razn):
+                    numFind2 = n 
+                    razn2 = (X-n)*-1
         else:
             numFind = n 
             razn = X-n
-    print(f"Число {numFind} ближайшее к {X}")
+    if razn<=razn2: # выберем наименьший по разнице отставания
+        print(f"Число {numFind} ближайшее к {X}")
+    else:
+        print(f"Число {numFind2} ближайшее к {X}")
 except:
     print("Упс, что-то пошло не так")
